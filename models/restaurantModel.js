4d8c4e93c141ebea1d5bfd6439b9dcfbbbba223b
Jamie1501 <jamie1501@hotmail.co.uk>
2022-05-03 03:37:29 +0100

    styled the add form and made it so dishes can only be displayed if they are available

--- models/restaurantModel.js
+++ models/restaurantModel.js
@@ -15,10 +15,11 @@ class Restaurant {
       dishName: "Testing 1",
       dishDescription:
         "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text.",
-      menuCategory: "lunch",
+      menuCategory: "Lunch",
       ingredients: "ingredient 1, ingredient 2, ingredient 3",
       allergies: "Nuts, Dairy",
       cost: 40,
+      isAvailable: true
     });
     console.log("db entry Testing 1 inserted");
 
@@ -26,10 +27,11 @@ class Restaurant {
       dishName: "Testing 2",
       dishDescription:
         "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text.",
-      menuCategory: "dinner",
+      menuCategory: "Dinner",
       ingredients: "ingredient 1, ingredient 2",
       allergies: "Dairy",
       cost: 25,
+      isAvailable: true
     });
     console.log("db entry Testing 2 inserted");
 
@@ -37,10 +39,11 @@ class Restaurant {
       dishName: "Testing 3",
       dishDescription:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      menuCategory: "dinner",
+      menuCategory: "Dinner",
       ingredients: "ingredient 1",
       allergies: "Nuts, Dairy",
       cost: 15,
+      isAvailable: true
     });
     console.log("db entry Testing 3 inserted");
 
@@ -48,10 +51,11 @@ class Restaurant {
       dishName: "Testing 4",
       dishDescription:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      menuCategory: "dinner",
+      menuCategory: "Dinner",
       ingredients: "ingredient 1, ingredient 2, ingredient 3",
       allergies: "Nuts, Dairy",
       cost: 15,
+      isAvailable: false
     });
     console.log("db entry Testing 4 inserted");
 
@@ -59,10 +63,11 @@ class Restaurant {
       dishName: "Testing 5",
       dishDescription:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      menuCategory: "dinner",
+      menuCategory: "Dinner",
       ingredients: "ingredient 1, ingredient 2, ingredient 3",
       allergies: "Nuts",
       cost: 15,
+      isAvailable: false
     });
     console.log("db entry Testing 5 inserted");
 
@@ -70,10 +75,11 @@ class Restaurant {
       dishName: "Testing 6",
       dishDescription:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      menuCategory: "lunch",
+      menuCategory: "Lunch",
       ingredients: "ingredient 1, ingredient 2, ingredient 3",
       allergies: "Nuts, Dairy",
       cost: 15,
+      isAvailable: true
     });
     console.log("db entry Testing 6 inserted");
   }
@@ -103,7 +109,7 @@ class Restaurant {
     return new Promise((resolve, reject) => {
       //use the find() function of the database to get the data,
       //error first callback function, err for error, entries for data
-      this.db.find({ menuCategory: "dinner"}, function (err, dinnerRecipes) {
+      this.db.find({ menuCategory: "Dinner", isAvailable: true}, function (err, dinnerRecipes) {
         //if error occurs then the promise will be rejected
         if (err) {
           reject(err);
@@ -123,7 +129,7 @@ class Restaurant {
     return new Promise((resolve, reject) => {
       //use the find() function of the database to get the data,
       //error first callback function, err for error, entries for data
-      this.db.find({ menuCategory: "lunch"}, function (err, lunchRecipes) {
+      this.db.find({ menuCategory: "Lunch", isAvailable: true}, function (err, lunchRecipes) {
         //if error occurs then the promise will be rejected
         if (err) {
           reject(err);
@@ -147,6 +153,7 @@ addRecipe(dishName, dishDescription, menuCategory, ingredients, allergies, cost)
     ingredients: ingredients,
     allergies: allergies,
     cost: cost,
+    isAvailable: true
           };
   console.log('entry created', entry);
   this.db.insert(entry, function(err, doc) {
